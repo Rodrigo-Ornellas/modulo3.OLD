4899416b0066a464de260f18c79a07e1f1b85b8d
Rodrigo Ornellas <rodrigo.ornellas@mail.com>
2012-03-22 21:08:47 -0300

    mudancas no settings e models para completar rename da app entrega

--- pizzaria/entrega/models.py
+++ pizzaria/entrega/models.py
@@ -6,11 +6,11 @@ class Cliente(models.Model):
     nome = models.CharField(max_length=128)
     fone = models.CharField(max_length=16)
     email = models.EmailField(blank=True)
-# blank = true significa que o campo é opcional logo é uma forma de validacao do formulario
-# null = true significa que este campo no BD pode aceitar null e para funcionar tem usar tambem o blank = true
-# toda classe do models.Model representara uma tabela na base de dados
-
+    
     def __unicode__(self):
         return self.nome
 
+# blank = true significa que o campo é opcional logo é uma forma de validacao do formulario
+# null = true significa que este campo no BD pode aceitar null e para funcionar tem usar tambem o blank = true
+# toda classe do models.Model representara uma tabela na base de dados
 
